05a7ac528d180cab6844d09fb965bee7b92064ed
SuyashSoundatte <suyashsoundatte10@gmail.com>
2024-10-26 22:40:44 +0530

    login and signup connected

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./NavBar.css"
+import "./NavBar.css";
 import { Outlet, Link } from 'react-router-dom'
 
 function NavBar() {
@@ -7,21 +7,29 @@ function NavBar() {
     <>
       <nav id="nav">
         {/* <div id="admin">Only for Admin</div> */}
-        <Link to={"/"}>
-          <p id="home">Home</p>
-        </Link>
-        <Link to={"/login"}>
-            <p id="login" >Login</p>
-          </Link>
-          <Link to={"/signup"}>
-            <p id="signup">SignUp</p>
-          </Link>
-          <Link to={"/student/register"}>
-            <p id="student">Student</p>
-          </Link>
-          <Link to={"/result"}>
-            <p id="result">Result</p>
-          </Link>
+          <div>
+            <Link to={"/"}>
+              <p id="home">Home</p>
+            </Link>
+          </div>
+          <div>
+            <Link to={"/login"}>
+              <p id="login" >Login</p>
+            </Link>
+            <Link to={"/signup"}>
+              <p id="signup">SignUp</p>
+            </Link>
+            <Link to={"/student/register"}>
+              <p id="student">Student</p>
+            </Link>
+            <Link to={"/result"}>
+              <p id="result">Result</p>
+            </Link>
+            <Link to={"/admin"}>
+              <p></p>
+            </Link>
+          </div>
+          
       </nav>
       <Outlet/>
     </>
