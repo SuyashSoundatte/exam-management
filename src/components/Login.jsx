773b8a7654915cb18778fb98528cd688975aa014
SuyashSoundatte <suyashsoundatte10@gmail.com>
2024-10-27 11:33:45 +0530

    login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function Login() {
   const [email, setEmail] = useState('');
@@ -50,6 +51,10 @@ function Login() {
         onChange={(e) => setPass(e.target.value)} 
       />
       <button id="login" onClick={handleLogin}>Login</button>
+      <p>Create an account</p>
+      <Link to={'/signup'}>
+        <button>SignUp</button>
+      </Link>
     </>
   )
 }
